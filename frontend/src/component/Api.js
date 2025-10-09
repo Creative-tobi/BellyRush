f68a8aecb3152575d5105436966c2f95524b0bdb
Creative-tobi <adeoyeaisha533@gmail.com>
2025-10-09 21:12:21 +0100

    merge

--- frontend/src/component/Api.js
+++ frontend/src/component/Api.js
@@ -1,10 +1,9 @@
 import axios from "axios";
 
-// export const BACKEND_BASE_URL = "http://localhost:5000";
 
 const Api = axios.create({
-  //   baseURL: "https://ticket-ordering.onrender.com/api",
-  baseURL: "http://localhost:5000",
+    baseURL: "https://bellyrush.onrender.com/api",
+  // baseURL: "http://localhost:5000/api",
 });
 
 Api.interceptors.request.use(
